8b48da1726df287a33bc7d9172d113a4da2a0bc2
Joe Sarro <jsarro@nd.edu>
2021-06-24 11:54:48 -0400

    June Updates

--- Generate_Genbank_Submission.py
+++ Generate_Genbank_Submission.py
@@ -2,13 +2,14 @@ from __future__ import print_function
 #!/usr/bin/python
 #Generate_Genbank_Submission.py 04-2021
 #Written by Joseph Sarro
-#This script splits generates a submission file for Genbank using the genbank submisison template .tsv from the Dragen run and another file contaiing the date of the survailance test.
+#This script generates a submission file for Genbank using the genbank submisison template .tsv from the Dragen run and another file contaiing the date of the survailance test.
+#Edit 05-2021 change input file from Dragen index to GISAID submission file to filter out bad samples 
 import sys
 import os
 #import the name of the fasta file associated with these sequences
 Fasta = sys.argv[1]
 fasta = os.path.splitext(Fasta)[0]
-#import the Dragen csv file
+#import the GISAID csv file
 temp = sys.argv[2]
 f = Fasta
 ff = open(temp,'r')
@@ -21,10 +22,11 @@ for lines in ff:
 	if first == True:
 		first = False
 	else:
-		fields = lines.split("	")
-		Id=fields[0]
-		field = Id.split("/")
-		ID=field[2]
+		fields = lines.split(",")
+		ID=fields[0]
+		#field = Id.split("/")
+		#ID=field[2]
+		#Id = "hCoV-19/USA/IN-",ID,"/2021"
 		Date=""
 		#this file must contain UND ID in first column and date in second
 		sur=open('/Users/joe/Covid_seq/Spreadsheets/Positives_Since_Jan_1.csv','r')
@@ -34,6 +36,6 @@ for lines in ff:
 			#print ID + IDZ
 			if ID == IDZ:
 				Date=fieldz[1]
-				print(Id,'	USA: Indiana, St. Joseph County	Homo sapiens	SARS-CoV-2/human/USA/',ID,'/2021	',Date,'	saliva',file=gis,sep='')
+				print(ID,'	USA: Indiana, St. Joseph County	Homo sapiens	SARS-CoV-2/human/USA/',ID,'/2021	',Date,'	saliva',file=gis,sep='')
 gis.close()
 ff.close()
--- Generate_State_Report.sh
+++ Generate_State_Report.sh
@@ -3,13 +3,13 @@
 #####edited 5-2021 to add date and pangloin version and remove osx created files
 ######Generate table to submit to state. Three report files are needed from panglolin, GISAID, and Nextclade.
 ###parse pangolin.csv
-awk -F ',' '{print $1","$2}' pangolin.csv > pangolin.tmp.csv
+awk -F ',' '{print $1","$2}' Dataexport.csv > pangolin.tmp.csv
 sort pangolin.tmp.csv > pangolin.tmp.sorted.csv
 rm pangolin.tmp.csv
 ###Parse gisaid table
 
 ###this file name will change with each run*********
-cp ../1620229268386.metadata.tsv metadata.working.tsv
+cp 1623249503732.metadata.tsv metadata.working.tsv
 ##########******************************************
 sed -i -e 's/hCoV-19\/USA\/IN-//g' "metadata.working.tsv"
 sed -i -e 's/\/2021//g' "metadata.working.tsv"
@@ -27,17 +27,19 @@ rm nextclade.filt.tmp.csv
 join -t , state.merge.1.csv nextclade.filt.tmp.sorted.csv >>  state.merge.2.csv
 rm state.merge.1.csv
 rm nextclade.filt.tmp.sorted.csv
-###Parse date files in Gisaid submission
-awk -F ',' '{print $6","$27}' 4-27-21_EpiCoV_BulkUpload.csv > date.tmp.csv
+###Parse date files in Gisaid submissioni
+####Change upload file
+awk -F ',' '{print $6","$27}' 210601_CovidSeqRun-06_EpiCoV_BulkUpload.csv > date.tmp.csv
+#####*****************
 sort -t , -k2 date.tmp.csv > date.sorted.csv
 rm date.tmp.csv
 
 #########edit date in output file for each run############
-echo "Barcode,Lineage,GISAID_ID,Clade,Date,Pangloin_Version" >  4-27-2021_test.state.csv
-join -t , -2 2 state.merge.2.csv date.sorted.csv >>  4-27-2021_test.state.csv
+echo "Barcode,Lineage,GISAID_ID,Clade,Date,Pangloin_Version" >  CovidSeqRun-06_06-3-2021.state.csv
+join -t , -2 2 state.merge.2.csv date.sorted.csv >>  CovidSeqRun-06_06-3-2021.state.csv
 rm state.merge.2.csv
 rm date.sorted.csv
-sed -i -e '2,$s/$/,v2.3.8/g' "4-27-2021_test.state.csv"
+sed -i -e '2,$s/$/,v.3.0.3/g' "CovidSeqRun-06_06-3-2021.state.csv"
 #####remove duplicate files due to sed osx extension
 rm *-e
 #Done!

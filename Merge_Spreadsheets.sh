88195c3bf6802e422385973bc758422516643b35
Joe Sarro <jsarro@nd.edu>
2021-06-24 13:08:33 -0400

    June Updates 3

--- Merge_Spreadsheets.sh
+++ Merge_Spreadsheets.sh
@@ -1,6 +1,3 @@
-#####Written by Josph Sarro#####
-##### 4-2021#######
-#####edited to add run number and remove OSX created files 5-2021#####
 ###parse Dragen info
 cp Dataexport.csv Dataexport.working.csv
 sed -i -e  's/"//g' "Dataexport.working.csv"
@@ -8,7 +5,7 @@ sed -i -e  's/(coverage)(amplicons)//g' "Dataexport.working.csv"
 sort -t , Dataexport.working.csv > Dataexport.sorted.csv
 rm Dataexport.working.csv
 ###Parse Ct info
-join -t , Dataexport.sorted.csv ../RNA_Var_sorted.parsed.csv > merge1.csv
+join -t , Dataexport.sorted.csv ../RNA_Var_updated_parsed.sorted.csv > merge1.csv
 ###parse probe info
 awk -F ',' '{print $1","$2","$5,","$6","$9","$12,","$13}' probes.csv > probes.parsed.csv
 sed -i -e  's/"//g' "probes.csv"
@@ -23,6 +20,6 @@ rm merge2.csv
 
 ###Add run number 
 ###Note****Change this number with every run 
-sed -i -e 's/$/,3/g' "All_fields_merged.csv"
+sed -i -e 's/$/,6/g' "All_fields_merged.csv"
 #####remove duplicate files due to sed osx extension
 rm *-e
